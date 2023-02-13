18ff162288834aa6c2e8039de1ad30528eb38b1f
Maegan Phan <maegyoung@gmail.com>
2023-02-13 10:51:34 -0600

    created event form and game form and added http requests to managers. Created all routes and buttons required for full form functionality. Tested and works.

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react"
 import { getEvents } from "../../managers/EventManager.js"
+import { useNavigate } from "react-router-dom"
 
 export const EventList = (props) => {
+    const navigate = useNavigate()
     const [ events, setEvents ] = useState([])
 
     useEffect(() => {
@@ -9,6 +11,12 @@ export const EventList = (props) => {
     }, [])
 
     return (
+        <>
+        <button className="btn btn-2 btn-sep icon-create"
+            onClick={() => {
+                navigate({ pathname: "/events/new" })
+            }}
+            >Create New Event</button>
         <article className="events">
             {
                 events.map(event => {
@@ -19,6 +27,6 @@ export const EventList = (props) => {
                     </section>
                 })
             }
-        </article>
+        </article></>
     )
 }
